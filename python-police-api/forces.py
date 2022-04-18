63ad81893b76462296ec9a072f85df61fc5c2707
Igor Loginov <igor.loginov@infrontx.com>
2022-04-18 12:29:29 +0400

    removing json module, changed shebang to python3, main function added.

--- python-police-api/forces.py
+++ python-police-api/forces.py
@@ -1,34 +1,36 @@
-#!/usr/bin/env python2
+#!/usr/bin/env python3
 import csv
-import json
 from police_api import PoliceAPI
 from police_api.forces import Force
 
-api = PoliceAPI()
-forces = api.get_forces()
+def main():
+    api = PoliceAPI()
+    forces = api.get_forces()
 
-# creating new csv file with structure.
-with open("forces.csv", "w") as file:
-    writer = csv.writer(file)
-    writer.writerow(
-        [
-            "force id",
-            "facebook page",
-        ]
-    )
+    # creating new csv file with structure.
+    with open("forces.csv", "w") as file:
+        writer = csv.writer(file)
+        writer.writerow(
+            [
+                "force id",
+                "facebook page",
+            ]
+        )
 
-for dep in forces:
-    # opening forces.csv in append mode.
-    f = open("forces.csv", "a")
-    force = Force(api, id=dep.id)
-    # getting engagements methods
-    json_engagements = force.engagement_methods
-    for data in json_engagements:
-        # searching for those who has facebook type of engagement
-        if data["type"] == "Facebook":
-            force_facebook = data["url"]
-            csv_data = [dep.id, force_facebook]
-            # appending to csv file.
-            with f:
-                writer = csv.writer(f)
-                writer.writerow(csv_data)
+    for dep in forces:
+        # opening forces.csv in append mode.
+        f = open("forces.csv", "a")
+        force = Force(api, id=dep.id)
+        # getting engagements methods
+        engagements = force.engagement_methods
+        for data in engagements:
+            # searching for those who has facebook type of engagement
+            if data["type"] == "Facebook":
+                force_facebook = data["url"]
+                csv_data = [dep.id, force_facebook]
+                # appending to csv file.
+                with f:
+                    writer = csv.writer(f)
+                    writer.writerow(csv_data)
+if __name__ == '__main__':
+    main()
